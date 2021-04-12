248cb1bbf24295d6ea38637498c49d3df3c5baf4
VladDishkant <vlad.dishkant111@gmail.com>
2021-04-12 22:14:25 +0300

    Finish project. Have been added mathod 'fillField()' and tests for it, tests for method 'parser()' for checking exceptions. Also have been added 'Tests.PNG' where you can see result of tests.

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 
+
+"""Parsing data
+    @params data -> our data from file
+
+    @return fieldSize, coords, field -> where:
+                                            fieldSize - size field Width and Height,
+                                            coords - start point for filling,
+                                            field - our field which we must filling"""
+
+
 def parser(data):
     fieldSize = []
     coords = []
@@ -25,7 +35,7 @@ def parser(data):
                     raise
         else:
             field.append([])
-            field[row-2].append(data[row])
+            field[row - 2].append(data[row])
 
             if len(field) > fieldSize[1]:
                 try:
@@ -35,16 +45,53 @@ def parser(data):
                     print('Has been exception with FIELD!')
                     raise
 
-            if len(field[row-2][0]) != fieldSize[0]:
+            if len(field[row - 2][0]) != fieldSize[0]:
                 try:
                     raise IOError('Width of {0} row is equal {1}, but must be {2}!'
-                                  .format(row-1, len(field[row-2][0]), fieldSize[0]))
+                                  .format(row - 1, len(field[row - 2][0]), fieldSize[0]))
                 except IOError:
                     print('Has been exception with FIELD!')
                     raise
     return fieldSize, coords, field
 
+
+"""Filling our field
+    @params field -> our field which we must filling
+    @params fieldSize -> size field Width and Height
+    @params fillingSymbol -> which symbol we must filling in the field
+    @params coords -> start point for filling"""
+
+
+def fillField(field, fieldSize, fillingSymbol, coords):
+    coords = list(map(lambda x: x - 1, coords))
+    unverifiedNeighbors = []
+
+    while True:
+        for i in [[0, -1], [0, 1], [-1, 0], [1, 0]]:
+            row = coords[1] + i[1]
+            column = coords[0] + i[0]
+
+            if row == -1 or column == -1 or row == fieldSize[1] or column == fieldSize[0]: continue
+
+            if field[row][0][column] == fillingSymbol:
+                field[row][0] = field[row][0][:column] + 'Z' + field[row][0][column + 1:]
+                unverifiedNeighbors.append([])
+                unverifiedNeighbors[len(unverifiedNeighbors) - 1].append(column)
+                unverifiedNeighbors[len(unverifiedNeighbors) - 1].append(row)
+        if len(unverifiedNeighbors) == 0:
+            break
+        else:
+            coords = unverifiedNeighbors.pop(len(unverifiedNeighbors) - 1)
+
+
+"""Starting point of the program"""
 if __name__ == "__main__":
     with open('inputData.txt', 'r', encoding='UTF-8') as file:
         inputData = file.read()
+    fillingSymbol = 'o'
     fieldSize, coords, field = parser(inputData.split('\n'))
+    print("Before:")
+    for i in field: print(i[0])
+    fillField(field, fieldSize, fillingSymbol, coords)
+    print("\nAfter:")
+    for i in field: print(i[0])
--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 import main
 
+
 def test_parser():
     inputData = """7 4
 4 2
@@ -13,3 +14,127 @@ oooCCoo"""
     assert fieldSize == [7, 4] \
            and coords == [4, 2] \
            and field == [['oooAAoo'], ['ooDooBo'], ['ooDooBo'], ['oooCCoo']]
+
+
+def test_parserWrongFieldSize():
+    inputData = """7 d
+    4 2
+    oooAAoo
+    ooDooBo
+    ooDooBo
+    oooCCoo"""
+
+    try:
+        main.parser(inputData.split('\n'))
+        assert False
+    except IOError:
+        assert True
+
+
+def test_parserWrongCoordsSize():
+    inputData = """7 4
+    4 Z
+    oooAAoo
+    ooDooBo
+    ooDooBo
+    oooCCoo"""
+
+    try:
+        main.parser(inputData.split('\n'))
+        assert False
+    except IOError:
+        assert True
+
+
+def test_parserWrongWidthField():
+    inputData = """7 4
+    4 2
+    oooAA
+    ooDooBo
+    ooDooBo
+    oooCCoo"""
+
+    try:
+        main.parser(inputData.split('\n'))
+        assert False
+    except IOError:
+        assert True
+
+
+def test_parserWrongHeightField():
+    inputData = """7 4
+    4 2
+    oooAAoo
+    ooDooBo
+    ooDooBo
+    oooCCoo
+    ooooooo"""
+
+    try:
+        main.parser(inputData.split('\n'))
+        assert False
+    except IOError:
+        assert True
+
+
+def test_fillNeighbors_firstCase():
+    inputData = """7 4
+4 2
+oooAAoo
+ooDooBo
+ooDooBo
+oooCCoo"""
+
+    fillingSymbol = 'o'
+    fieldSize, coords, field = main.parser(inputData.split('\n'))
+    main.fillField(field, fieldSize, fillingSymbol, coords)
+    assert field == [['oooAAoo'], ['ooDZZBo'], ['ooDZZBo'], ['oooCCoo']]
+
+
+def test_fillNeighbors_secondCase():
+    inputData = """7 4
+4 2
+oooXXoo
+ooXooXo
+ooXooXo
+ooXooXo"""
+
+    fillingSymbol = 'o'
+    fieldSize, coords, field = main.parser(inputData.split('\n'))
+    main.fillField(field, fieldSize, fillingSymbol, coords)
+    assert field == [['oooXXoo'], ['ooXZZXo'], ['ooXZZXo'], ['ooXZZXo']]
+
+
+def test_fillNeighbors_thirdCase():
+    inputData = """7 4
+4 2
+oooXXoo
+ooXooXo
+oooooXo
+ooXooXo"""
+
+    fillingSymbol = 'o'
+    fieldSize, coords, field = main.parser(inputData.split('\n'))
+    main.fillField(field, fieldSize, fillingSymbol, coords)
+    assert field == [['ZZZXXoo'], ['ZZXZZXo'], ['ZZZZZXo'], ['ZZXZZXo']]
+
+
+def test_fillNeighbors_fourthCase():
+    inputData = """10 10
+1 1
+oooooooooo
+ooXooooooo
+oXoXoooooo
+ooXooooooo
+oooooooooo
+oooooXXXoo
+oooooXooXo
+oooooXooXo
+oooooXoXoo
+oooooooooo"""
+
+    fillingSymbol = 'o'
+    fieldSize, coords, field = main.parser(inputData.split('\n'))
+    main.fillField(field, fieldSize, fillingSymbol, coords)
+    assert field == [['ZZZZZZZZZZ'], ['ZZXZZZZZZZ'], ['ZXoXZZZZZZ'], ['ZZXZZZZZZZ'], ['ZZZZZZZZZZ'],
+                     ['ZZZZZXXXZZ'], ['ZZZZZXZZXZ'], ['ZZZZZXZZXZ'], ['ZZZZZXZXZZ'], ['ZZZZZZZZZZ']]
